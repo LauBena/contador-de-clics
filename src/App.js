45a1136053996c60bfbc594d87743e5e5eb54105
LauBena <87872842+LauBena@users.noreply.github.com>
2022-05-29 18:43:29 -0300

    modificamos parte del codigo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import './App.css';
-import freecodeCampLogo from './imagenes/freecodecamp-logo.png';
+import Boton from '../src/componentes/boton';
+import Contador from './componentes/contador';
+import { useState } from 'react';
 
 function App() {
+
+  const [numClics, setnumClics] = useState(0);
+
+  const manejarClic = () => {
+    setnumClics(numClics + 1);
+  };
+
+  const reiniciarContador = () => {
+    setnumClics(0);
+  };
+
   return (
     <div className="App">
-      <div className='freecodecamp-logo-contenedor'>
-        <img 
-        className='freecodecamp-logo'
-        src={freecodeCampLogo}
-        alt='Logo'/>
-      </div>
       <div className='contenedor-principal'>
-        
+        <Contador numClics={numClics} />
+        <Boton
+        texto='Clic'
+        botonDeClic={true}
+        manejarClic={manejarClic}/>
+        <Boton
+        texto='Reiniciar'
+        botonDeClic={false}
+        manejarClic={reiniciarContador}/>
       </div>
     </div>
   );
