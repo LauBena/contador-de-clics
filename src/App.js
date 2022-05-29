93bb6abb99036304aa3b8d06db57cdf429f6f367
LauBena <87872842+LauBena@users.noreply.github.com>
2022-05-29 17:30:51 -0300

    importamos el logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import freecodeCampLogo from './imagenes/freecodecamp-logo.png';
 
 function App() {
   return (
     <div className="App">
-
+      <div className='freecodecamp-logo-contenedor'>
+        <img 
+        className='freecodecamp-logo'
+        src={freecodeCampLogo}
+        alt='Logo'/>
+      </div>
+      <div className='contenedor-principal'>
+        
+      </div>
     </div>
   );
 }
